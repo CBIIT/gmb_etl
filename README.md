1e92ff586c5c2ce864f88ca8689eafc99eb3202a
Fuyuan Wang <wfy199719971997@gmail.com>
2022-04-13 11:30:00 -0400

    Script for extracting data from Rave.

---
+++ README.md
@@ -0,0 +1,4 @@
+# gmb-etl extraction script
+Code to extract data from Rave.<br/>
+To run the python script for extracting Gmb data to the local machine, the user can use the command<br/>
+```python3 gmb-etl.py auth_gmb.yaml```<br/>
---
+++ gmb-etl.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import requests
+from requests.auth import HTTPBasicAuth
+import yaml
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
+import argparse
+
+# In[2]:
+
+
+######GET DATASET FROM RAVE######
+print('GET DATASET FROM RAVE')
+parser = argparse.ArgumentParser()
+parser.add_argument('auth_file')
+args = parser.parse_args()
+auth = args.auth_file
+# auth = 'auth_gmb.yaml'
+with open(auth) as f:
+    auth = yaml.load(f, Loader = yaml.FullLoader)
+r = requests.get(auth['API'], auth = HTTPBasicAuth(auth['USERNAME'], auth['PASSWORD']))
+data_set = r.content.decode("utf-8")
+data = BeautifulSoup(data_set, features = 'lxml')
+
+
+# In[3]:
+
+
+######TRANSFORM DATASET######
+print('TRANSFORM DATASET')
+data_dict = {}
+for clinicaldata in data.odm:
+    node_name = clinicaldata.subjectdata.studyeventdata.formdata['formoid']
+    for itemdata in clinicaldata.subjectdata.studyeventdata.formdata.itemgroupdata:
+        if node_name not in data_dict.keys():
+            data_dict[node_name] = {}
+        if itemdata['itemoid'] not in data_dict[node_name].keys():
+            data_dict[node_name][itemdata['itemoid']] = []
+        try:
+            data_dict[node_name][itemdata['itemoid']].append(itemdata['value'])
+        except:
+            data_dict[node_name][itemdata['itemoid']].append(None)
+
+
+# In[4]:
+
+
+######PRINT DATA FILES######
+print('PRINT DATA FILES')
+for node_type in data_dict:
+    df = pd.DataFrame()
+    for node_key in data_dict[node_type]:
+        df[node_key] = data_dict[node_type][node_key]
+    file_name = auth['OUTPUT_FOLDER'] + node_type + ".tsv"
+    if not os.path.exists(auth['OUTPUT_FOLDER']):
+        os.mkdir(auth['OUTPUT_FOLDER'])
+    df.to_csv(file_name, sep = "\t", index = False)
+
+
+# In[5]:
+
+
+######VALIDATE DATA FILES######
+print('VALIDATE DATA FILES')
+with open(auth['NODE_FILE']) as f:
+    model = yaml.load(f, Loader = yaml.FullLoader)
+for node in model['Nodes']:
+    if node not in data_dict.keys():
+        print(f'Data node {node} is not in the dataset')
+
+for node in model['Nodes']:
+    if node in data_dict.keys():
+        for prop in model['Nodes'][node]['Props']:
+            prop_name = node + '.' +  prop
+            if prop_name not in data_dict[node].keys():
+                print(f'Property {prop_name} is not in the dataset')
+
+
+# In[ ]:
+
+
+
+
