c5ac219821be6ae559c9a95e2a5cf8b5600d8a25
Fuyuan Wang <wfy199719971997@gmail.com>
2022-05-03 13:43:08 -0400

    add rename_properties and remove_properties functions

--- gmb-transformation.py
+++ gmb-transformation.py
@@ -6,8 +6,6 @@ import os
 import yaml
 import random
 import boto3
-import datetime
-import dateutil.tz
 import argparse
 
 
@@ -19,7 +17,7 @@ config = args.config_file
 #config = 'gmb_config.yaml'
 
 
-#download file from s3
+#Download file from s3
 def download_from_s3(s3, args_s3_sub_folder):
     subfolder = 'Raw/' + args_s3_sub_folder
     subfolder_dirsctory = './' + args_s3_sub_folder + '/'
@@ -31,14 +29,14 @@ def download_from_s3(s3, args_s3_sub_folder):
             if not os.path.exists(subfolder_dirsctory):
                 os.mkdir(subfolder_dirsctory)
             s3.download_file(config['S3_BUCKET'], key['Key'], file_key)
-#upload files to s3
+#Upload files to s3
 def upload_files(s3, config, timestamp):
     for file_name in os.listdir(config['OUTPUT_NODE_FOLDER']):
         if file_name.endswith('.tsv'):
             file_directory = config['OUTPUT_NODE_FOLDER'] + file_name
             s3_file_directory = 'Transformed' + '/' + timestamp + '/' + file_name
             s3.upload_file(file_directory ,config['S3_BUCKET'], s3_file_directory)
-#add id field for data file
+#Add id field for data file
 def add_id_field(parent_node, df, file_name):
     if file_name[0] != parent_node:
         node_id_number_list = random.sample(range(10**5, 10**6), len(df))
@@ -47,13 +45,33 @@ def add_id_field(parent_node, df, file_name):
         id_key = file_name[0] + '_id'
         df[id_key] = node_id_number_list
     return df
-#transform node name
-def transform_node_name(old_node_name, new_node_name, df, file_name):
+#Rename node
+def rename_node(old_node_name, new_node_name, df, file_name):
     if file_name[0] == old_node_name:
         file_name[0] = new_node_name
         type_list = [new_node_name] * len(df)
         df['type'] = type_list
     return df, file_name
+#Rename properties
+def rename_properties(old_property, new_property, df):
+    for x in range(0, len(old_property)):
+        df = df.rename(columns={old_property[x]: new_property[x]})
+    return df
+#Remove properties that are not in the data_node file
+def remove_properties(df, node, node_name):
+    remove_list = []
+    property_list = df.columns.tolist()
+    for property in property_list:
+        if property not in node['Props'] and property != 'type' and '.' not in property:
+            remove_list.append(property)
+    if len(remove_list) == 0:
+        print(f'Data node {node_name} does not have any properties to remove')
+    else:
+        df = df.drop(columns = remove_list)
+        print(f'Data node {node_name} removes {remove_list}')
+    return df
+            
+
 
 with open(config) as f:
     config = yaml.load(f, Loader = yaml.FullLoader)
@@ -68,12 +86,17 @@ for file_name in os.listdir(download_file_directory):
     if file_name.endswith(".tsv"):
         df = pd.read_csv(os.path.join(download_file_directory, file_name), sep='\t')
         file_name = file_name.split('.')
-        df, file_name = transform_node_name('PHYSICAL_EXAM___SCREENING', 'PHYSICAL_EXAM_SCREENING', df, file_name)
+        #Rename node
+        df, file_name = rename_node('PHYSICAL_EXAM___SCREENING', 'PHYSICAL_EXAM_SCREENING', df, file_name)
         if file_name[0] in model['Nodes'].keys():
             df = add_id_field('SUBJECT', df, file_name)
-            # rename SubjectKey
-            df = df.rename(columns={'SubjectKey': 'SUBJECT.PT_ID'})
-            # Print the data
+            #Rename SubjectKey
+            old_property = ['SubjectKey', 'REG_INST_ID_CD_ENR', 'GRMLN_VAR_PTHGNC_CAT']
+            new_property = ['SUBJECT.PT_ID','REG_INST_ID_CD','SOMATIC_VAR_PTHGNC_CAT']
+            df = rename_properties(old_property, new_property, df)
+            #Remove property
+            df = remove_properties(df, model['Nodes'][file_name[0]], file_name[0])
+            #Print the data
             file_name = config['OUTPUT_NODE_FOLDER'] + file_name[0] + '.tsv'
             if not os.path.exists(config['OUTPUT_NODE_FOLDER']):
                 os.mkdir(config['OUTPUT_NODE_FOLDER'])
